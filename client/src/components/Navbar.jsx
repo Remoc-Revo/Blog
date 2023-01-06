7e34300bd92efe060892a05d7aed1bf16843aa7f
Tim <timothynguono@gmail.com>
2023-01-06 23:30:35 +0300

    implemented basic navbar responsiveness

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,13 +8,13 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
-      <div className="container">
+      <div className="container" >
         <div className="logo">
           <Link to="/">
           <img src={Logo} alt="" />
           </Link>
         </div>
-        <div className="links">
+        <div className="links" id="horizontal-navbar">
           <Link className="link" to="/?cat=art">
             <h6>ART</h6>
           </Link>
@@ -22,7 +22,6 @@ const Navbar = () => {
             <h6>SCIENCE</h6>
           </Link>
           <Link className="link" to="/?cat=technology">
-            <h6>TECHNOLOGY</h6>
           </Link>
           <Link className="link" to="/?cat=cinema">
             <h6>CINEMA</h6>
